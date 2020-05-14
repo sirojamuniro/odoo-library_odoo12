d7e7ee6da58f762f4d61b4413550dad9b55cca6d
sirojamuniro <munirosiroja@gmail.com>
2020-05-14 10:13:40 +0700

    First Documentation

---
+++ __init__.py
@@ -0,0 +1,12 @@
+{
+'name': "Librariku",
+'summary': "Managemen Book",
+'description': """Library cok""",
+'author': "Siroja Muniro",
+'website': "www.rojaweb.000webhostapp.com",
+'category': 'Uncategorized',
+'version': '12.0.1',
+'depends': ['base'],
+'data': ['views.xml'],
+'demo': ['demo.xml'],
+}
---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+from . import models1
+from . import models2
+from . import models3
+from . import models4
+from . import models5
